8ab3185ac9ce7df2ad3c9f48fb3b45b6307896ec
AbbieWills <119050905+AbbieWills@users.noreply.github.com>
2023-09-04 12:01:25 +0100

    adding correct pages

--- client/pages/HomePage/HomePage.jsx
+++ client/pages/HomePage/HomePage.jsx
@@ -1,79 +1,73 @@
 import React from 'react'
 import './homepage.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faPassport, faRoute, faLeaf, faGift, faBook, faGlobeAmericas, } from '@fortawesome/free-solid-svg-icons';
-import world from './world.png'
-import peaks from './peaks.png'
+import { faLifeRing, faLocationDot, faShip, faTruckFast, faSterlingSign, faCalendarCheck } from '@fortawesome/free-solid-svg-icons';
+import ImageCarousel from '../../components/Carousel/carousel.jsx';
 
-export default function HomePage() {
+
+const HomePage = () => {
   return (
     <>
-    <div className="container" data-testid="title_container">
+    <div data-testid="title_container">
     <div className="background-home">
-      <img className='peaks' src={peaks} alt="Waves" />
     </div>
       <div className="row intro-explain-container">
-          <h1 className='intro' data-testid = "home_title">Travelling doesn't have to cost the earth!</h1>
-          <div className="col-md-8 text-column">
-            <p className='explain' data-testid = "home_description">Welcome to Travel-Wise, your compass to responsible and sustainable travel! <br></br> Our planet's well-being is paramount, this site sets out to empower
-              travellers with the tools and knowledge to make eco-conscious choices that leave a positive impact on the environment.</p>
-          </div>
-        <div className="col-md-4 image-column">
-          <img src={world} alt="globe" className="column-image" data-testid="globe_image"/>
-        </div>
+          <h1 className='intro' data-testid = "home_title">Life-Jacket Servicing!</h1>
+          <ImageCarousel />
+            <p className='explain' data-testid = "home_description">At Kemps MPS, we service life jackets as we understand the importance of keeping your 
+      life jackets in peak condition.</p>
       </div>
     </div>
-    <div className="container mt-5" data-testid="icons_container">
+    <div className="features" data-testid="icons_container">
       <div className="row">
         <div className="col-md-4" data-testid="explore_container">
-          <div className="feature-card text-center p-4">
-            <FontAwesomeIcon icon={faPassport} size="3x" className="mb-3 pass" data-testid="explore_icon"/>
-            <h4 className='iconTitle' data-testid="explore_title" >Explore the World</h4>
-            <p data-testid="explore_description">Discover new destinations and experience cultures from around the globe.</p>
+          <div className="feature-card text-center p-5">
+            <FontAwesomeIcon icon={faLifeRing} size="3x" className="mb-3 ring" data-testid="explore_icon"/>
+            <h4 className='iconTitle' data-testid="explore_title" >Safety</h4>
+            <p data-testid="explore_description">When it comes to your safety on the water, there's no room for compromise. 
+    </p>
           </div>
         </div>
         <div className="col-md-4" data-testid="routes_container">
           <div className="feature-card text-center p-4">
-            <FontAwesomeIcon icon={faRoute} size="3x" className="mb- route" data-testid="routes_icon" />
-            <h4 className='iconTitle' data-testid="routes_title">Eco-Friendly Routes</h4>
-            <p data-testid="routes_description">Find the most sustainable travel routes and reduce your carbon footprint.</p>
+            <FontAwesomeIcon icon={faLocationDot} size="3x" className="mb- route" data-testid="routes_icon" />
+            <h4 className='iconTitle' data-testid="routes_title">Ideal Location</h4>
+            <p data-testid="routes_description">Located close to Hull, East Yorkshire. We are in the optimal location for all your needs!</p>
           </div>
         </div>
         <div className="col-md-4" data-testid="sustainable_container">
           <div className="feature-card text-center p-4">
-            <FontAwesomeIcon icon={faLeaf} size="3x" className="mb-3 leaf" data-testid="sustainable_icon"/>
-            <h4 className='iconTitle' data-testid="sustainable_title">Sustainable Practices</h4>
-            <p data-testid="sustainable_description">Get tips on eco-friendly accommodations, transportation, and responsible tourism.</p>
+            <FontAwesomeIcon icon={faCalendarCheck} size="3x" className="mb-3 calendar" data-testid="sustainable_icon"/>
+            <h4 className='iconTitle' data-testid="sustainable_title">Quick Service</h4>
+            <p data-testid="sustainable_description">Add some text about quick and good service.</p>
           </div>
         </div>
+        <div className="col-md-4" data-testid="sustainable_container">
+          <div className="feature-card text-center p-4">
+            <FontAwesomeIcon icon={faTruckFast} size="3x" className="mb-3 truck" data-testid="sustainable_icon"/>
+            <h4 className='iconTitle' data-testid="sustainable_title">Quick Turnaround</h4>
+            <p data-testid="sustainable_description">Add some text about quick delivery and collection.</p>
+            </div>
+            </div>
+            <div className="col-md-4" data-testid="sustainable_container">
+          <div className="feature-card text-center p-4">
+            <FontAwesomeIcon icon={faSterlingSign} size="3x" className="mb-3 pound" data-testid="sustainable_icon"/>
+            <h4 className='iconTitle' data-testid="sustainable_title">Competitive Pricing</h4>
+            <p data-testid="sustainable_description">Add some text about pricing.</p>
+            </div>
+            </div>
+            <div className="col-md-4" data-testid="sustainable_container">
+          <div className="feature-card text-center p-4">
+            <FontAwesomeIcon icon={faShip} size="3x" className="mb-3 ship" data-testid="sustainable_icon"/>
+            <h4 className='iconTitle' data-testid="sustainable_title">Experienced</h4>
+            <p data-testid="sustainable_description">Add some text about experience.</p>
+            </div>
+            </div>
       </div>
-      <div className="row mt-4">
-        <div className="col-md-4" data-testid="rewards_container">
-          <div className="feature-card text-center p-4">
-            <FontAwesomeIcon icon={faGift} size="3x" className="mb-3 gift" data-testid="rewards_icon"/>
-            <h4 className='iconTitle' data-testid="rewards_title">Eco-Friendly Rewards</h4>
-            <p data-testid="rewards_description">Earn rewards for making environmentally conscious choices during your travels.</p>
-          </div>
-        </div>
-        <div className="col-md-4" data-testid="greener_container">
-          <div className="feature-card text-center p-4">
-          <FontAwesomeIcon icon={faGlobeAmericas} size="3x" className="mb-3 globe" data-testid="greener_icon"/>
-            <h4 className='iconTitle' data-testid="greener_title">A Greener Tomorrow</h4>
-            <p data-testid="greener_description">Join us in preserving the planet for future generations through responsible travel.</p>
-          </div>
-        </div>
-        <div className="col-md-4" data-testid="diary_container">
-          <div className="feature-card text-center p-4">
-          <FontAwesomeIcon icon={faBook} size="3x" className="mb-3 book" data-testid="diary_icon"/>
-            <h4 className='iconTitle' data-testid="diary_title">Personal Travel Diary</h4>
-            <p data-testid="diary_description">Document your eco-friendly experiences and memories in your travel diary.</p>
-          </div>
-        </div>
       </div>
-    </div>
- 
     </>
-      
     
   )
 }
+
+export default HomePage
--- client/pages/JourneyPage/JourneyPage.jsx
+++ client/pages/JourneyPage/JourneyPage.jsx
@@ -1,40 +1,40 @@
-import React, { useState } from "react";
-import styles from "./JourneyPage.css";
-import EcoFriendlySuggestions from "../../components/JourneyComponents/EcoFriendlySuggestions";
-import DonateModal from "../../components/JourneyComponents/DonateModal";
-import { faMoneyCheckDollar } from '@fortawesome/free-solid-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import React from 'react';
+import './JourneyPage.css';
+import waves from './Wave.svg'
 
-const JourneyPage = () => {
-  const [isModalOpen, setIsModalOpen] = useState(false);
-
-  const openModal = () => {
-    setIsModalOpen(true);
-  };
-
-  const closeModal = () => {
-    setIsModalOpen(false);
-  };
+const AboutPage = () => {
 
   return (
-    <div className='background' data-testid = "background">
-    <div className={styles.containerWrapper}>
-      <h2 className="journey-intro" data-testid = "journey_title">Plan your eco-friendly journey now!</h2>
-      <div className="jp-container-wrapper">
-        <div className="jp-top">
-          <div className="jp-logo"></div>
-        </div>
-        <div className="jp-journey-form">
-          <EcoFriendlySuggestions />
-          <button onClick={openModal} className="btn btn-success donate-button" data-testid = "donate_button">
-            Donate to offset your carbon footprint <FontAwesomeIcon icon={faMoneyCheckDollar} />
-          </button>
+    <>
+        <div className='background'>
+      <img className='waves' src={waves} alt="Waves" />
+      </div>
+      <div className="about">
+        <h1>About Us</h1>
+        <p>
+          Welcome to Kemps MPS, your trusted partner for life jacket servicing. We understand the
+          importance of safety on the water, and that's why we're committed to keeping your life jackets
+          in peak condition.
+        </p>
+        <p>
+          With years of experience in the industry, our team of experts is dedicated to ensuring that
+          your life jackets meet the highest safety standards. We offer comprehensive servicing and
+          inspection to make sure your life jackets are ready for any adventure.
+        </p>
+        <p>
+          At Kemps MPS, safety is our top priority. Whether you're a recreational boater, a fisherman,
+          or an adventure seeker, you can trust us to provide reliable and professional life jacket
+          servicing.
+        </p>
+        <p> <a className='contact-link' href='/contact'>Contact us</a> today to learn more about our services and how we can help you stay safe on the water.</p>
+        <div className='service-text'>
+        <h2>The service</h2>
+        <p>All life jackets are serviced same etc. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
         </div>
       </div>
-      <DonateModal isOpen={isModalOpen} onRequestClose={closeModal} />
-    </div>
-    </div>
+
+    </>
   );
 };
 
-export default JourneyPage;
+export default AboutPage;
