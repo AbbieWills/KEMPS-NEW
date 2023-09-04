ff4e44391d9b02f0ef97f0a1f5dbf9112933c751
AbbieWills <119050905+AbbieWills@users.noreply.github.com>
2023-09-04 19:40:54 +0100

    permissions express fixed

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,6 @@
 import express from 'express';
 import { createTransport } from 'nodemailer';
 import { json } from 'body-parser';
-import axios from 'axios';
-require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 3001;
 
