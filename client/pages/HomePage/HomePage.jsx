050af8c6fa340515a17d78eed7644c9021002c9f
AbbieWills <119050905+AbbieWills@users.noreply.github.com>
2023-09-06 14:55:06 +0100

    responsive design

--- client/pages/HomePage/HomePage.jsx
+++ client/pages/HomePage/HomePage.jsx
@@ -8,8 +8,6 @@ const HomePage = () => {
   return (
     <>
     <div>
-    <div className="background-home">
-    </div>
       <div className="row intro-explain-container">
           <h1 className='intro'>Life Jacket Servicing</h1>
           <ImageCarousel />
@@ -21,54 +19,54 @@ const HomePage = () => {
     </div>
     <div className="features">
       <div className="row">
-        <div className="col-md-4">
+        <div className="col-12 col-sm-6 col-md-4">
           <div className="feature-card text-center p-5">
             <FontAwesomeIcon icon={faLifeRing} size="3x" className="mb-3 ring"/>
             <h4 className='iconTitle'>Safety</h4>
             <p>Safety is our top priority. Whether you are a recreational boater, a fisherman,
           or an adventure seeker, you can trust us to provide reliable and professional life jacket
-          servicing. 
-    </p>
+          servicing. </p>
           </div>
         </div>
-        <div className="col-md-4">
+        <div className="col-12 col-sm-6 col-md-4">
           <div className="feature-card text-center p-4">
             <FontAwesomeIcon icon={faLocationDot} size="3x" className="mb-3 route"/>
             <h4 className='iconTitle'>Ideal Location</h4>
             <p>Located close to Hull, East Yorkshire. We are in the optimal location for all your needs!</p>
           </div>
         </div>
-        <div className="col-md-4">
+        <div className="col-12 col-sm-6 col-md-4">
           <div className="feature-card text-center p-4">
             <FontAwesomeIcon icon={faCalendarCheck} size="3x" className="mb-3 calendar"/>
             <h4 className='iconTitle'>Quick Service</h4>
             <p >Add some text about quick and good service.</p>
           </div>
         </div>
-        <div className="col-md-4" >
+        <div className="col-12 col-sm-6 col-md-4" >
           <div className="feature-card text-center p-4">
             <FontAwesomeIcon icon={faTruckFast} size="3x" className="mb-3 truck" />
             <h4 className='iconTitle' >Quick Turnaround</h4>
             <p>Add some text about quick delivery and collection.</p>
-            </div>
-            </div>
-            <div className="col-md-4" >
+          </div>
+        </div>
+        <div className="col-12 col-sm-6 col-md-4" >
           <div className="feature-card text-center p-4">
             <FontAwesomeIcon icon={faSterlingSign} size="3x" className="mb-3 pound"/>
             <h4 className='iconTitle' >Competitive Pricing</h4>
             <p>Add some text about pricing.</p>
-            </div>
-            </div>
-            <div className="col-md-4">
+          </div>
+        </div>
+        <div className="col-12 col-sm-6 col-md-4">
           <div className="feature-card text-center p-4">
             <FontAwesomeIcon icon={faShip} size="3x" className="mb-3 ship"/>
             <h4 className='iconTitle'>Experienced</h4>
             <p>With years of experience in the industry, our team of experts is dedicated to ensuring that
           your life jackets meet the highest safety standards.</p>
-            </div>
-            </div>
+          </div>
+        </div>
       </div>
       </div>
+
     </>
     
   )
--- client/pages/ServicesPage/ServicesPage.jsx
+++ client/pages/ServicesPage/ServicesPage.jsx
@@ -13,40 +13,42 @@ const ServicesPage = () => {
         <h1 className='servicing-title'>Servicing and Pricing</h1>
         <h2>Finding The Right Jacket For You </h2>
         <p className='right-jacket-text'>Determining the ideal lifejacket for your needs requires thoughtful consideration. Below is a helpful guide to assist you in making the right selection. </p>
-
           <p className='sizing-guidelines'> Sizing Guidelines: </p>
           <p className='sizing-text'>
           The majority of level 150 and level 275 inflatable lifejackets are manufactured in a single size, typically designed for adults weighing over 40kg. However, it is crucial to refer to the manufacturer's specifications for the specific weight range accommodated by each lifejacket model.<br/>
-          <p className='sizing-guidelines'> Understanding Lifejacket Standards: </p>
+          <p className='understanding'> Understanding Lifejacket Standards: </p>
           All new lifejackets sold in the UK and Europe must adhere to ISO 12402 standards.
           Lifejackets produced prior to the introduction of ISO 12402 standards must meet the EN393 to EN399 standards. Deciphering ISO 12402:
           ISO 12402 establishes international standards for personal flotation devices, encompassing both life jackets and buoyancy aids.</p>
-  
         <div className='row levels-section'>
           <h2 className='levels-title'>Exploring Life Jacket Levels </h2>
-          <div className='col-lg-4'>
+          <div className='individual'>
             <p className='levels'>Level 100:</p>
             <p className='levels-text'>
             This life jacket is suitable for calm, flat waters. It may not possess the capability to right an individual solely through its buoyancy force. Level 100 lifejackets are typically non-SOLAS approved.</p> <img className='lj-photo' src={hundered} alt="100-lf" />
           </div>
-          <div className='col-lg-4 onefive'>
+          <div className='individual'>
           <p className='levels'>Level 150:</p>
           <p className='levels-text'>
               The standard life jacket for use in coastal and offshore waters. Level 150 lifejackets are designed to high standards and can effectively right an unconscious person based on buoyancy alone. Note that the thickness and weight of clothing worn may influence its performance.</p> <img className='lj-photo' src={hundredfifty} alt="150-lf" />
           </div>
-          <div className='col-lg-4'>
+          <div className='individual'>
           <p className='levels'>Level 275+:</p>
           <p className='levels-text'>
             This life jacket is intended for rugged offshore conditions. Similar to the Level 150, it can right an unconscious person, even when wearing heavy clothing such as immersion suits.</p> <img className='lj-photo' src={twohundredseventyfive} alt="275-lf" />
           </div>
         </div>
-        <div className='pricing' >
-        <h2>Pricing</h2>
-        <p className='price-list'>  
-          -Solas approved lifejacket service- £20 (this doesn't include the cost of any replaceable parts)
-          <br/>-non-solas lifejacket service- £15 (this doesn't include the cost of any replaceable parts)
-        </p>
-       </div>
+       <div className='pricing'>
+          <h2>Pricing</h2>
+          <ul className='price-list'>
+            <li>
+              <strong>Solas approved lifejacket service:</strong> £20 (excluding the cost of any replaceable parts)
+            </li>
+            <li>
+              <strong>Non-Solas lifejacket service:</strong> £15 (excluding the cost of any replaceable parts)
+            </li>
+          </ul>
+        </div>
 
         <p className='contact-link-text'> <a className='contact-link' href='/contact'>Contact us</a> today to learn more about our services and how we can help you stay safe on the water.</p>
     
